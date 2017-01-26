a26752b64afff16062491869bc9e75fecefdc5f7
Mrinal Wadhwa <mwadhwa@fybr-tech.com>
2017-01-26 10:40:21 +0530

    bump version to v0.1.6

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExCov.Mixfile do
 
   def project do
     [app: :excov,
-     version: "0.1.5",
+     version: "0.1.6",
      description: description(),
      deps: deps(),
      package: package()]
